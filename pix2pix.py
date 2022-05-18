abaafabacf90cd3be10fcbdfec0680dfe5dae5fb
Yuliya smolkina <smolkina.djulia@gmail.com>
2022-05-18 09:19:46 +0300

    Update pix2pix.py
    225  #tf.train.string_input_producer ->tf.data.Dataset.from_tensor_slices

--- pix2pix.py
+++ pix2pix.py
@@ -223,7 +223,7 @@ def load_examples():
         input_paths = sorted(input_paths)
 
     with tf.name_scope("load_images")
-        path_queue = tf.data.Dataset.from_tensor_slices(input_paths, shuffle=a.mode == "train") #tf.train.string_input_producer ->tf.data.Dataset.from_tensor_slices  
+        path_queue = tf.data.Dataset.from_tensor_slices(input_paths, shuffle=a.mode == "train") 
         reader = tf.WholeFileReader()
         paths, contents = reader.read(path_queue)
         raw_input = decode(contents)
